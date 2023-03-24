7110859a23318b3360f345e5645978e40d934828
Jeffrey191997 <kgmyatun97@gmail.com>
2023-03-24 18:46:33 +0700

    changing list with mui

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile (){
+    return(
+        <div className="layout">
+            <img src="assets/Jeff.JPG" alt="profile"/>
+            <h1 className="name">Jeffrey</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
