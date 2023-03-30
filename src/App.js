1c2a1be7cb9a08cf1b241f0e4d5e7483c0448930
Jeffrey191997 <kgmyatun97@gmail.com>
2023-03-30 12:22:21 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
-import Home from "./components/Home";
-import Todo from "./components/Todo";
+import HomePage from "./pages/HomePage";
 import TodoPage from "./pages/TodoPage";
+import TodayPage from "./pages/TodayPage"
+import Calendar from "./pages/Calendar"
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    // <div className="App">
-    //   <div className="home">
-    //     <Home />
-    //   </div>
-    //   <div className="todo">
-    //     <Todo />
-    //   </div>
-    // </div>
-    <TodoPage />
+    <Routes>
+      <Route exect path="/" element={<HomePage />} />
+      <Route path="/todo" element={<TodoPage />} />
+      <Route path="/today" element={<TodayPage />} />
+      <Route path="/calendar" element={<Calendar />} />
+    </Routes>
+
   );
 }
 
